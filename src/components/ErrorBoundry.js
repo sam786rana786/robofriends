6c80afdf4f0618dce98023553be41b736103e15b
Munish Rana <sam786rana786@gmail.com>
2018-11-18 06:21:37 +0530

    Created robofriends app

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+    render() {
+        return 
+    }
+}
+export default ErrorBoundry;
